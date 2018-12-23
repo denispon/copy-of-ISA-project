240e9dc93c061eab71e9c17e730f526629ec0b63
Sale1996 <ftnmejl96@gmail.com>
2018-12-23 12:10:47 +0100

    Added skeleton of purchases microservice

---
+++ purchasesMicroservice/purchasesCore/src/main/java/com/ftn/isa/projekat/purchases/purchasesCore/PurchasesApplication.java
@@ -0,0 +1,17 @@
+package com.ftn.isa.projekat.purchases.purchasesCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class PurchasesApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PurchasesApplication.class, args);
+
+
+	}
+
+}
