819300b9e4ffbc243c920b5d3801af757a115330
MarkoKatic96 <katicmarko96@gmail.com>
2019-01-29 03:04:55 +0100

    Povezivanje fronta i backenda za izlistavanje hotela

--- hotelMicroservice/hotelCore/src/main/java/com/ftn/isa/projekat/hotel/hotelCore/Hotel/controller/HotelController.java
+++ hotelMicroservice/hotelCore/src/main/java/com/ftn/isa/projekat/hotel/hotelCore/Hotel/controller/HotelController.java
@@ -4,8 +4,10 @@ import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,8 +50,10 @@ public class HotelController {
 							 @ApiResponse( code = 404, message ="Not Found")})	
 	public ResponseEntity<List<HotelDTO>> getAllHotels(){
 		
+		HttpHeaders head = new HttpHeaders();
+		head.add("Access-Control-Allow-Origin", "*");
 		List<HotelDTO> hotels = hotelService.findAll();
-		return(!hotels.isEmpty()) ? new ResponseEntity<List<HotelDTO>>(hotels, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		return(!hotels.isEmpty()) ? new ResponseEntity<List<HotelDTO>>(hotels, head, HttpStatus.OK) : new ResponseEntity<>(head, HttpStatus.NOT_FOUND);
 		
 	}
 	
