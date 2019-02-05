76f8b01fdec5acdc7e2220732659dc38217b6f4f
Sale1996 <ftnmejl96@gmail.com>
2019-02-05 14:45:29 +0100

    Popravljen bag kod prikaza invitacija

--- isa-front/src/components/dashboard/invitation/Invitation.js
+++ isa-front/src/components/dashboard/invitation/Invitation.js
@@ -1,14 +1,22 @@
 import React from "react"
 import ReservationInfo from "../reservation/ReservationInfo";
 
+
+//<ReservationInfo></ReservationInfo>
 const Invitation = () => {
 
     return (
 
         <div className="card">
             <div className="card-content">
-                <span className="card-title indigo-text lighten-1"><strong>Branko je kreirao rezervaciju za Vas:</strong></span>
-                <ReservationInfo></ReservationInfo>
+                <span className="card-title indigo-text lighten-1"><strong>Veljko je kreirao rezervaciju za Vas:</strong></span>
+                <div className="container left">
+                    <div className="card">
+                        <div className="card-content">
+                            <h3>Ovde bi trebala ici rezervacija</h3>
+                        </div>
+                    </div>
+                </div>
                 <div className="row">
                     <div className="col s1">
                         <button className="btn  indigo lighten-1">Prihvati</button>
